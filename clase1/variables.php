c40fa7e6cfe2b08771368d76cd97fc81120adc08
Marcos <exegeses@gmail.com>
2021-08-12 22:06:33 -0300

    Variables y constantes en PHP

---
+++ clase1/variables.php
@@ -0,0 +1,29 @@
+<?php
+    $numero = 147;
+    $texto = 'cadena de caractéres';
+    const NOMBRE = 'Rick';
+    /* const APELLIDO = 'Sánchez'; */
+    define( 'APELLIDO', 'Sánchez' );
+    echo $numero;
+?>
+<br>
+<?php
+    $numero2 = 12;
+    echo $numero + $numero2;
+?>
+<br>
+<?php
+    //no necesito concatenar
+    echo NOMBRE .' '. APELLIDO;
+?>
+    <br>
+<?php
+    //solo imprimimos
+    echo NOMBRE, ' ', APELLIDO;
+?>
+<br>
+<?php
+    $filtro = 2000;
+    $sql = "SELECT precio FROM productos
+                    WHERE precio < ".$filtro;
+?>
