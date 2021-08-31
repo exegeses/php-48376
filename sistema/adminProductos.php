ce81292c6f379b7730cf8a34743c46e18ecee928
Marcos <exegeses@gmail.com>
2021-08-30 21:52:46 -0300

    Panel de administración de productos

--- sistema/adminProductos.php
+++ sistema/adminProductos.php
@@ -1,6 +1,8 @@
 <?php  
 
-
+    require 'funciones/conexion.php';
+    require 'funciones/productos.php';
+    $productos = listarProductos();
 	include 'includes/header.html';  
 	include 'includes/nav.php';  
 ?>
@@ -30,15 +32,15 @@
             </thead>
             <tbody>
 <?php
-
+        while( $producto = mysqli_fetch_assoc( $productos ) ){
 ?>            
                 <tr>
-                    <td>producto</td>
-                    <td>Precio</td>
-                    <td>Marca</td>
-                    <td>Categoria</td>
-                    <td>Presentación</td>
-                    <td>Imagen</td>
+                    <td><?= $producto['prdNombre'] ?></td>
+                    <td>$<?= $producto['prdPrecio'] ?></td>
+                    <td><?= $producto['mkNombre'] ?></td>
+                    <td><?= $producto['catNombre'] ?></td>
+                    <td><?= $producto['prdPresentacion'] ?></td>
+                    <td><img src="productos/<?= $producto['prdImagen']; ?>" class="img-thumbnail"></td>
                     <td>
                         <a href="" class="btn btn-outline-secondary">
                             Modificar
@@ -51,7 +53,7 @@
                     </td>
                 </tr>
 <?php
-
+        }
 ?>
             </tbody>
         </table>
--- sistema/funciones/productos.php
+++ sistema/funciones/productos.php
@@ -5,7 +5,26 @@
 
     function listarProductos()
     {
-
+        $link = conectar();
+        /* $sql = "SELECT
+                    idProducto, prdNombre, prdPrecio, 
+                    mkNombre, catNombre, 
+                    prdPresentacion, prdStock, prdImagen
+                FROM productos, marcas, categorias
+                WHERE productos.idMarca = marcas.idMarca
+                  AND productos.idCategoria = categorias.idCategoria";
+        */
+        $sql = 'SELECT 
+                        idProducto, prdNombre, prdPrecio, 
+                        mkNombre, catNombre,
+                        prdPresentacion, prdStock, prdImagen 
+                    FROM productos 
+                        join marcas 
+                            on productos.idMarca = marcas.idMarca 
+                        join categorias 
+                            on productos.idCategoria = categorias.idCategoria';
+        $productos = mysqli_query( $link, $sql );
+        return $productos;
     }
     function verProductoPorID()
     {}
