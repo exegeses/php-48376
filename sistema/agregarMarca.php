7923f656cd8a760cd80b7f0d5bd531a211b1e23a
Marcos <exegeses@gmail.com>
2021-09-02 21:52:39 -0300

    Resolución mensaje de alta ok
    Utilizamos un flag (true o false) para setear valor de variables que muetran color de css y mensaje de reporte en el div

--- sistema/agregarMarca.php
+++ sistema/agregarMarca.php
@@ -2,31 +2,24 @@
 
     require 'funciones/conexion.php';
     require 'funciones/marcas.php';
-    $check = agregarMarca();
+    $flag = agregarMarca();
+    $css = 'danger';  //color rojo
+    $mensaje = 'No se puede agregar la marca'; //predeterminado
+    if ( $flag ) {
+        $css = 'success'; //color verde
+        $mensaje = 'Marca agregada correctamente';
+    }
 	include 'includes/header.html';  
 	include 'includes/nav.php';  
 ?>
 
     <main class="container">
         <h1>Alta de una marca</h1>
-<?php
-    if ( $check ){
-?>
-     <div class="alert alert-success">
-         Marca agregada correctamente.
-         <a href="adminMarcas.php" class="btn btn-light">Volver a panel</a>
-     </div>
-<?php
-    }
-    else{
-?>
-        <div class="alert alert-danger">
-            No se pudo agregar la marca.
-            <a href="adminMarcas.php" class="btn btn-light">Volver a panel</a>
+
+        <div class="alert alert-<?= $css ?>">
+            <?= $mensaje ?>
+            <a href="adminMarcas.php" class="btn btn-light">Volver a panel </a>
         </div>
-<?php
-    }
-?>
 
     </main>
 
