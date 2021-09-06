5481ec6256ce5a6cac4355b74c160196d6a4cb40
Marcos <exegeses@gmail.com>
2021-09-06 19:25:07 -0300

    Dashboard

---
+++ sistema/admin.php
@@ -0,0 +1,31 @@
+<?php  
+	
+	include 'includes/header.html';  
+	include 'includes/nav.php';  
+?>
+
+    <main class="container">
+        <h1>Dashboard</h1>
+
+        <div class="list-group">
+            <a href="adminMarcas.php"
+               class="list-group-item list-group-item-action">
+               Panel de administración de marcas.
+            </a>
+            <a href="adminCategorias.php"
+               class="list-group-item list-group-item-action">
+               Panel de administración de categorías.
+            </a>
+            <a href="adminProductos.php"
+               class="list-group-item list-group-item-action">
+               Panel de administración de productos.
+            </a>
+            <a href="adminUsuarios.php"
+               class="list-group-item list-group-item-action">
+               Panel de administración de usuarios.
+            </a>
+        </div>
+
+    </main>
+
+<?php  include 'includes/footer.php';  ?>
