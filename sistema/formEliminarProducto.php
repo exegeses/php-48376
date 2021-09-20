678d49552f67aeb6aadb8b6416dbb1201480779d
Marcos <exegeses@gmail.com>
2021-09-20 19:21:51 -0300

    formulario de confirmación de baja de un producto

---
+++ sistema/formEliminarProducto.php
@@ -0,0 +1,52 @@
+<?php  
+
+    require 'funciones/conexion.php';
+    require 'funciones/productos.php';
+    $producto = verProductoPorID();
+	include 'includes/header.html';  
+	include 'includes/nav.php';  
+?>
+
+    <main class="container">
+        <h1>Baja de un producto</h1>
+
+        <article class="card border-danger py-3 col-6 mx-auto">
+            <div class="row">
+                <div class="col">
+                    <img src="productos/<?= $producto['prdImagen'] ?>" class="img-thumbnail">
+                </div>
+                <div class="col text-danger">
+                    <h2><?= $producto['prdNombre'] ?></h2>
+                    <?= $producto['catNombre'] ?> | <?= $producto['mkNombre'] ?>
+                    <br>
+                    $<?= $producto['prdPrecio'] ?>
+                    <br>
+                    <?= $producto['prdPresentacion'] ?>
+
+                    <form action="eliminarProducto.php" method="post">
+                        <input type="hidden" name="idProducto"
+                               value="<?= $producto['idProducto'] ?>">
+                        <button class="btn btn-danger btn-block my-3">
+                            Confirmar baja
+                        </button>
+                        <a href="adminProductos.php" class="btn btn-outline-secondary btn-block">
+                            Volver a panel
+                        </a>
+
+                    </form>
+                    
+                </div>
+            </div>
+        </article>
+
+        <script>
+        /*    Swal.fire(
+                'Advertencia',
+                'Si pulsa el botón "Confirmar baja", se eliminará el producto.',
+                'warning'
+            )*/
+        </script>
+
+    </main>
+
+<?php  include 'includes/footer.php';  ?>
